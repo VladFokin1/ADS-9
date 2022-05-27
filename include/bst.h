ff814aad2400ede7e96c8b16e098b8291eec6bf1
VladFokin1 <94217312+VladFokin1@users.noreply.github.com>
2022-05-27 09:24:41 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -3,32 +3,31 @@
 #define INCLUDE_BST_H_
 
 template < typename T >
-class BST
-{
-public :
+class BST {
+ public :
   struct Node {
     T value;
     int count;
     Node * left;
     Node * right;
   };
-  private :
+ private :
     Node * root;
     Node * addNode(Node * root , T value) {
       if (root == nullptr) {
-        root = new Node ;
-        root−>value = value ;
-        root−>count =1;
-        root−>left = root−>right = nullptr ;
+        root = new Node;
+        root −> value = value;
+        root −> count =1;
+        root −> left = root−>right = nullptr;
       } else if (root−>value > value) {
-        root−>left = addNode (root−>left, value) ;
-      } else if (root−>value < value) {
-        root−>right = addNode (root−>right, value) ;
+        root −> left = addNode (root −> left, value);
+      } else if (root −> value < value) {
+        root −> right = addNode (root −> right, value);
       } else
         root−>count++;
-      return root ;
+      return root;
     }
-  public :
+public :
     void add(T value) {
       root = addNode(root, value);
     } 
