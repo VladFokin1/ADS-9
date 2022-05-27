edd177aed1c094ea6f51cddd02fa30c490cbefc0
VladFokin1 <94217312+VladFokin1@users.noreply.github.com>
2022-05-27 09:18:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,12 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  BST<std::string> statistic_tree;
+  char sym;
+  while (!file.eof()) {
+    sym = file.get();
+    sym = tolower(sym);
+    if (sym >= 'a' && sym <= 'z') statistic_tree.add(sym);
+  }
 }
